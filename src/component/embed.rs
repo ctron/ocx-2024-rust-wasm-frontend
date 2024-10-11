6d365126f23224ba968d9f6760f2ec3d9c57f486
Jens Reimann <ctron@dentrassi.de>
2024-10-11 12:25:22 +0200

    chore: focus stealing doesn't work

--- src/component/embed.rs
+++ src/component/embed.rs
@@ -1,6 +1,4 @@
-use web_sys::HtmlElement;
 use yew::prelude::*;
-use yew_hooks::use_interval;
 
 #[derive(Properties, PartialEq)]
 pub struct EmbedProperties {
@@ -9,27 +7,7 @@ pub struct EmbedProperties {
 
 #[function_component(Embed)]
 pub fn embed(props: &EmbedProperties) -> Html {
-    let outer = use_node_ref();
-
-    {
-        let outer = outer.clone();
-        use_interval(
-            move || {
-                if let Some(ele) = outer.cast::<HtmlElement>() {
-                    if let Err(err) = ele.focus() {
-                        web_sys::console::log_2(&"Enable to focus:".into(), &err);
-                    }
-                } else {
-                    web_sys::console::log_1(&"Element not found".into());
-                }
-            },
-            1000,
-        );
-    }
-
     html!(
-        <div ref={outer}>
-            <iframe class="ocx-embed" src={&props.src}/>
-        </div>
+        <iframe class="ocx-embed" src={&props.src}/>
     )
 }
