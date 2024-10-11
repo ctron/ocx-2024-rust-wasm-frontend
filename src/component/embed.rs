e8fcb17d1ca3732aeef5d4a51033ab52c8550959
Jens Reimann <ctron@dentrassi.de>
2024-10-11 12:16:16 +0200

    chore: try stealing the focus

--- src/component/embed.rs
+++ src/component/embed.rs
@@ -1,4 +1,6 @@
+use web_sys::HtmlElement;
 use yew::prelude::*;
+use yew_hooks::use_interval;
 
 #[derive(Properties, PartialEq)]
 pub struct EmbedProperties {
@@ -7,7 +9,27 @@ pub struct EmbedProperties {
 
 #[function_component(Embed)]
 pub fn embed(props: &EmbedProperties) -> Html {
+    let outer = use_node_ref();
+
+    {
+        let outer = outer.clone();
+        use_interval(
+            move || {
+                if let Some(ele) = outer.cast::<HtmlElement>() {
+                    if let Err(err) = ele.focus() {
+                        web_sys::console::log_2(&"Enable to focus:".into(), &err);
+                    }
+                } else {
+                    web_sys::console::log_1(&"Element not found".into());
+                }
+            },
+            1000,
+        );
+    }
+
     html!(
-        <iframe class="ocx-embed" src={&props.src}/>
+        <div ref={outer}>
+            <iframe class="ocx-embed" src={&props.src}/>
+        </div>
     )
 }
